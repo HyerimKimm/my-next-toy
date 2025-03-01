56c405f19263bf1d98d6b9a58980521c824e6728
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-03-01 16:47:16 +0900

    Feat. better sqlite 3 세팅

---
+++ app/login/page.tsx
@@ -0,0 +1,33 @@
+import styles from "./page.module.css";
+
+export default function LoginPage() {
+  return (
+    <div className={styles.page}>
+      <form className={styles.form}>
+        <h2>로그인</h2>
+        <label htmlFor="id" className={styles.input_wrap}>
+          <div>아이디</div>
+          <input
+            id="id"
+            type="text"
+            placeholder="아이디를 입력해 주세요."
+            required
+          />
+        </label>
+        <label htmlFor="password" className={styles.input_wrap}>
+          <div>비밀번호</div>
+          <input
+            id="password"
+            type="password"
+            placeholder="비밀번호를 입력해 주세요."
+            required
+          />
+        </label>
+        <div className={styles.buttons_wrap}>
+          <button className={styles.login_button}>로그인</button>
+          <button className={styles.sign_up_button}>회원가입</button>
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ initdb.ts
@@ -0,0 +1,14 @@
+const sql = require("better-sqlite3");
+const db = sql("user.db");
+
+db.prepare(
+  `
+    CREATE TABLE IF NOT EXISTS users (
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       email TEXT UNIQUE NOT NULL,
+       password TEXT NOT NULL,
+       username TEXT NOT NULL,
+       created_date TEXT NOT NULL
+    )
+  `
+).run();
